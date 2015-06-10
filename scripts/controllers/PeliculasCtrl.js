fc57d49f6dce5ab36e2f615f252229ed873cf3c6
David de Tena <daviddetenacalvo@gmail.com>
2015-06-10 23:45:23 +0200

    Añadimos consumo de servicio de API para películas

--- scripts/controllers/PeliculasCtrl.js
+++ scripts/controllers/PeliculasCtrl.js
@@ -4,16 +4,29 @@
 //en controller va el nombre del controlador. Todo controlador recibe un $scope
 angular
     .module("imdbcutre")
-    .controller("PeliculasCtrl",["$scope",function($scope){
+    .controller("PeliculasCtrl",["$scope","$http",function($scope,$http){
 
-        // Colgamos en $scope para que la vista lo tenga disponible
-        $scope.elementos = [{
-            nombre: "The Matrix"
-        },
-        {
-            nombre: "The Matrix Mala"
-        },
-        {
-            nombre: "The Matrix Mala 2"
-        }];
+        // URL API (siguientes peliculas de estreno)
+        var url = "https://api.themoviedb.org/3/movie/upcoming";
+
+        // API key
+        url+= "?api_key=826b523c417cbb888744b13031d846c2";
+
+        // Idioma info
+        url+= "&language=es";
+
+        // Solicitamos un servicio a la API rest
+        $http
+            .get(url)
+            .then(
+                // La cosa ha ido bien
+                function(datos){
+                    $scope.peliculas = datos.data.results;
+                    //debugger;
+                },
+                // Algo ha ido a mal
+                function(){
+                    alert("Algo ha ido mal");
+                });
+
     }]);
